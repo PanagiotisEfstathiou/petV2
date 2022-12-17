2b707b27f5c1bc3c750693d392a45154a244e032
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-12-17 11:44:12 +0200

    Deletes some files

--- src/main/java/com/edu/petv2/service/SitterService.java
+++
@@ -1,9 +0,0 @@
-package com.edu.petv2.service;
-
-import com.edu.petv2.model.Sitter;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SitterService extends BaseService<Sitter>{
-
-}
