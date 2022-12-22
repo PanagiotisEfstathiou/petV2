71d2f2d1b44823a6b3bc4b235bd33a981bda5d64
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-12-22 15:50:14 +0200

    Implements filtering functionality for the sitters

--- src/main/java/com/edu/petv2/repository/SitterRepository.java
+++ src/main/java/com/edu/petv2/repository/SitterRepository.java
@@ -1,9 +1,15 @@
 package com.edu.petv2.repository;
 
+import com.edu.petv2.dto.SitterDto;
 import com.edu.petv2.model.Sitter;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SitterRepository extends JpaRepository<Sitter,Long> {
+    @Query("select o from Sitter o where o.petsAllowed = :petsAllowed")
+    List<Sitter> getSitterBy(@Param("petsAllowed") String petsAllowed);
 }
